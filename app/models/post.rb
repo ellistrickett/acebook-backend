f39602b299ed6e30eef046d425eab6753e2cb05c
Ellis Trickett <ellis.trickett@gmail.com>
2020-06-09 14:53:26 +0100

    fixed merge

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :user
 
-  def as_json(options={})
-   super(only: [:id, :message, :created_at, :user_id],
-   include: [user: { only: [:username] }]
-     )
+  def as_json(_options = {})
+    super(only: [:id, :message, :created_at, :user_id],
+    include: [user: { only: [:username] }]
+      )
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200606201208) do
+ActiveRecord::Schema.define(version: 2) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,4 +32,5 @@ ActiveRecord::Schema.define(version: 20200606201208) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
