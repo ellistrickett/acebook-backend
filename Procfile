c18c22b77426a77c6c68dd1ca5cd1d315e017912
Ellis Trickett <ellis.trickett@gmail.com>
2020-06-09 15:37:51 +0100

    intial attempt implementation of CORS

--- Procfile
+++ Procfile
@@ -1,3 +1,6 @@
+release: rake db:drop
+
+release: rake db:create
 
 release: rake db:migrate
 
--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,12 @@ module Acebook
      # config.autoload_paths << Rails.root.join('lib')
     config.eager_load_paths << Rails.root.join('lib')
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
