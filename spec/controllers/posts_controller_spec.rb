79ce374ec7963e71f182dbe81cc9435fb708ee68
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-06-10 18:48:43 +0100

    WIP - passing user controller tests

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,36 +1,84 @@
-require 'rails_helper'
-require 'json_helper'
-
-RSpec.describe PostsController, type: :controller do
-  # describe "POST /new not logged in" do
-  #   it "responds with 401" do
-  #     post :new
-  #     expect(response).to have_http_status(401)
-  #   end
-  # end
-# 
-#   describe "POST /" do
-#     it "responds with posts array" do
-#       post :create, params: { post: { user_id: 1, message: "Hello, world!" } }
-#       expect(JSON.parse(response.body)).to eq([ { "id" => post.id,
-#           "message" => post.message, 
-#                                               "created_at" => post.created_at.as_json, 
-#                                               "updated_at" => post.updated_at.as_json, 
-#                                               "user" => {  handle" => post.username  }])
-#     end
-#   end
+# require 'rails_helper'
 #
-
-  #   it "creates a post" do
-  #     post :create, params: { post: { message: "Hello, world!" } }
-  #     expect(Post.find_by(message: "Hello, world!")).to be
-  #   end
-  # end
-
-  # describe "GET /" do
-  #   it "responds with 200" do
-  #     get :index
-  #     expect(response).to have_http_status(200)
-  #   end
-  # end
-end
+#
+# RSpec.describe PostsController, type: :controller do
+#
+#   let(:valid_attributes) {
+#     { user_id: 1, message: "Hello!" }
+#   }
+#
+#   let(:post_attributes) {
+#     { email:"will@makers.com", username: "w1ll", name: "Will", password: "12345" ,password_confirmation: "12345"}
+#   }
+#
+#   describe "GET #index" do
+#       it "returns a JSON list of posts" do
+#         post :create, params: {user: post_attributes}, format: :json
+#         post = Post.create valid_attributes
+#         post_2 = Post.create valid_attributes.merge(message: "Hello 2")
+#         get :index, params: {}
+#         expect(response).to be_successful
+#         expect(JSON.parse(response.body)).to eq([
+#           {
+#             "id" => post_2.id,
+#             "body" => post_2.message,
+#             "created_at" => post_2.created_at.as_json,
+#             "updated_at" => post_2.updated_at.as_json,
+#             "user" => {
+#               "id" => post_2.user.id,
+#               "handle" => post_2.user.name
+#             },
+#
+#           },
+#           {
+#             "id" => post.id,
+#             "body" => post.message,
+#             "created_at" => post.created_at.as_json,
+#             "updated_at" => post.updated_at.as_json,
+#             "user" => {
+#               "id" => post.user.id,
+#               "handle" => post.user.handle
+#             },
+#
+#           }
+#         ])
+#       end
+#     end
+#
+#
+# #
+# #
+# #
+# #
+# #   describe "POST /new not logged in" do
+# #     it "responds with 401" do
+# #       post :new
+# #       expect(response).to have_http_status(401)
+# #     end
+# #   end
+# #
+# #   describe "POST /" do
+# #     it "responds with posts array" do
+# #       post :create, params: { post: { user_id: 1, message: "Hello, world!" } }
+# #       expect(JSON.parse(response.body)).to eq([ { "id" => post.id,
+# #           "message" => post.message, 
+# #                                               "created_at" => post.created_at.as_json, 
+# #                                               "updated_at" => post.updated_at.as_json, 
+# #                                               "user" => {  handle" => post.username  }])
+# #     end
+# #   end
+# #
+# #
+# #     it "creates a post" do
+# #       post :create, params: { post: { message: "Hello, world!" } }
+# #       expect(Post.find_by(message: "Hello, world!")).to be
+# #     end
+# #   end
+# #
+# #   describe "GET /" do
+# #     it "responds with 200" do
+# #       get :index
+# #       expect(response).to have_http_status(200)
+# #     end
+# #   end
+# end
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe UserController, type: :controller do
+
+  let(:valid_attributes) {
+    { email:"will@makers.com", username: "w1ll", name: "Will", password: "12345" ,password_confirmation: "12345"}
+  }
+
+  let(:invalid_attributes) {
+   {email:nil, username: "w1ll", name: "Will", password: "12345" ,password_confirmation: "12345" }
+  }
+
+  describe "User #create" do
+    context "with valid params" do
+      it "creates a new User" do
+        expect {
+          post :create, params: {user: valid_attributes}, format: :json
+        }.to change(User, :count).by(1)
+      end
+
+      it "renders a JSON response with the new user" do
+        post :create, params: {user: valid_attributes}, format: :json
+        expect(response).to have_http_status(:success)
+        expect(response.content_type).to eq('application/json')
+        user = User.last
+        expect(JSON.parse(response.body)).to include({
+          # "created_at" => user.created_at,
+          "username" => user.username
+        })
+      end
+    end
+  end
+
+    context "with invalid params" do
+      it "renders a JSON response with errors for the new user" do
+        post :create, params: {user: invalid_attributes}, format: :json
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.content_type).to eq('application/json')
+      end
+    end
+
+    context "with a duplicate username/email" do
+      it "renders a JSON response with errors for the new user" do
+        post :create, params: {user: valid_attributes}, format: :json
+        post :create, params: {user: valid_attributes}, format: :json
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.content_type).to eq('application/json')
+      end
+    end
+  end
