c6f8018ed29d7a214b8d2ab1632a3b7d79b68174
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-06-10 15:03:49 +0100

    WIP - removed old files, added update method to posts/routes.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,37 @@
 class PostsController < ApplicationController
   skip_before_action :authenticate_request, only: [:index]
-  
+
   def new
     @post = Post.new
   end
 
   def create
     @post = Post.create(post_params)
-    
+
     respond_to do |format|
       if @post.save
         format.json { render json: @post }
       else
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
-    end    
+    end
   end
 
   def index
     @posts = Post.all
     render json: @posts
   end
+
+
+  def update
+   respond_to do |format|
+     if @post.update(user_params)
+       format.json { render json: @post }
+     else
+       format.json { render json: @user.errors, status: :unprocessable_entity }
+     end
+   end
+ end
 
   private
 
