f25ce6276d2469da1e6c1c4858a50fee332f6fe3
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-06-10 16:31:35 +0100

    WIP - updated update method (pseudo code)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,9 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
   def index
     @posts = Post.all
     render json: @posts
@@ -24,14 +27,22 @@ class PostsController < ApplicationController
 
 
   def update
-   respond_to do |format|
-     if @post.update(user_params)
-       format.json { render json: @post }
-     else
-       format.json { render json: @user.errors, status: :unprocessable_entity }
+   @post = Post.find(params[:id])
+   if user_id = current_user.id
+     respond_to do |format|
+       if @post.update(update_params)
+         format.json { render json: @post }
+       else
+         format.json { render json: @post.errors, status: :unprocessable_entity }
+       end
      end
+   else
+     format.json { render json: @post.errors, status: :unprocessable_entity }
    end
  end
+
+  def destroy
+  end
 
   private
 
@@ -39,4 +50,16 @@ class PostsController < ApplicationController
     params.require(:post).permit(:message).merge(user_id: current_user.id)
   end
 
+  def update_params
+      params.require(:post).permit(:message)
+  end
+
 end
+
+# eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE1OTE4ODUyMzd9.po8TIc24gS5HL0iydycZxvn-fbWtOjg2QrT-gcdIJLQ
+
+# curl -H "Authorization: eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE1OTE4ODU3MTN9.9GMUPyuTrBD4mQraThRMD0qLVzYwM5tYqfnJ6zYMa_A" -H "Content-Type: application/json" -H "Accept: application/json" -X POST -d '{"post":{"message":"hello,world!"}}' http://localhost:3000/new
+#
+# curl -H "Authorization: eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE1OTE4ODUzOTB9.208HifaTNHK2xJOtKWyjDE5z7drtiGa9puU-SWuOd_I" -H "Content-Type: application/json" -H "Accept: application/json" -X POST -d '{"post":{"message":"hello,world!"}}' http://localhost:3000/new
+#
+# curl -H "Content-Type: application/json" -X POST -d '{"email":"email","password":"12345"}' http://localhost:3000/login
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   post 'login', to: 'authentication#authenticate'
   post 'new', to: 'posts#create'
   post 'signup', to: 'user#create'
-  post 'edit', to: 'post#update'
+  # post 'edit', to: 'post#update'
   resources :posts
 
 end
