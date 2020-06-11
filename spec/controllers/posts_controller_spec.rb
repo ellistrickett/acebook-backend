a384d0588c6fa725388dda9ac03b2e0d2e14cfce
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-06-11 14:07:08 +0100

    WIP - commit to enable change over

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -4,29 +4,18 @@ require 'rails_helper'
 RSpec.describe PostsController, type: :controller do
 
   describe "GET #index" do
-    it "upon initialize it returns an empty JSON array " do
+    it "upon initialization it returns an empty JSON array " do
       get :index, params: {}
       expect(response).to be_successful
       expect(JSON.parse(response.body)).to eq([])
     end
+
+
+    # it "it creates a new post" do
+    #   expect {
+    #       post :create, params: {post: {message:"hello, world!"}}, format: :json
+    #   }.to change(Post, :count).by(1)
+    # end
+
   end
-
-  # it "it returns a JSON list of posts" do
-  #   post :create, params: {post: valid_attributes}, format: :json
-  #   # Post.create valid_attributes
-  #   # post_2 = Post.create valid_attributes.merge(message: "Hello 2")
-  #   get :index, params: {}
-  #   expect(response).to be_successful
-  #   expect(JSON.parse(response.body)).to eq([
-  #     {
-  #       "id" => 1,
-  #       "body" => "Hello!",
-  #       "created_at" => "",
-  #       "updated_at" => "",
-  #
-  #       }
-  #     ])
-  #   end
-
-
 end
